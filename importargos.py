ee5a253995121b58672f3d68cf429b611951b856
jcf55 <jcf55@duke.edu>
2021-10-19 16:18:18 -0400

    Creates an empty feature class

--- importargos.py
+++ importargos.py
@@ -7,38 +7,34 @@
 ## Usage: ImportArgos <ARGOS folder> <Output feature class> 
 ##
 ## Created: Fall 2021
-## Author: jcf55@duke.edu  (for ENV859)
+## Author: jcf55@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
 
 # Import modules
 import sys, os, arcpy
 
+#Allow outputs to be overwritten
+arcpy.env.overwriteOutput = True
+
 # Set input variables (Hard-wired)
-#these are absolute paths; meaning it works for only us not everyone
 inputFile = 'D:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "D:/ARGOSTracking/Scratch/ARGOStrack.shp"
 
-#%% Construct a while loop to iterate through all lines in the datafile
-# Open the ARGOS data file for reading
-# Import modules
-import sys, os, arcpy
+# Create feature class to which we will add features
+outPath, outFile = os.path.split(outputFC)
+arcpy.management.CreateFeatureclass(outPath, outFile)
 
-# Set input variables (Hard-wired)
-inputFile = '../Data/ARGOSData/1997dg.txt'
-outputFC = '../Scratch/ARGOStrack.shp'
-
-## Construct a while loop to iterate through all lines in the datafile
-# Open the ARGOS data file for reading
+#%% Construct a while loop and iterate through all lines in the data file
+# Open the ARGOS data file
 inputFileObj = open(inputFile,'r')
 
-# Get the first line of data, so we can use a while loop
+# Get the first line of data, so we can use the while loop
 lineString = inputFileObj.readline()
+
+#Start the while loop
 while lineString:
     
-   #We have 7 lines of information that we want to skip before we start read
-    #Set the code to run only if the line contains the string "Date :"
-    #This means if Date : is in the line that we are reading....
-    #code below that is indented will only be run if this is true
+    # Set code to run only if the line contains the string "Date: "
     if ("Date :" in lineString):
         
         # Parse the line into a list
@@ -46,11 +42,7 @@ while lineString:
         
         # Extract attributes from the datum header line
         tagID = lineData[0]
-        obsDate = lineData[3]
-        obsTime = lineData[4]
-        obsLC = lineData[7]
         
-        #Because this data is on a different line we need it to read this
         # Extract location info from the next line
         line2String = inputFileObj.readline()
         
@@ -60,9 +52,14 @@ while lineString:
         # Extract the date we need to variables
         obsLat = line2Data[2]
         obsLon= line2Data[5]
+                    
+        # Extract the date, time, and LC values
+        obsDate = lineData[3]
+        obsTime = lineData[4]
+        obsLC   = lineData[7]
         
         # Print results to see how we're doing
-        print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
+        print (tagID,"Lat:"+obsLat,"Long:"+obsLon, obsLC, obsDate, obsTime)
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
